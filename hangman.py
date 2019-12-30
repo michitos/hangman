17a8e80ed77494e7f8e1e1652e56b02831148704
michitos <michitos1143@gmail.com>
2019-12-30 22:56:00 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,45 @@
+# http://tinyurl.com/jhrvs94
+def hangman(word):
+    wrong = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|       |       ",
+             "|       0       ",
+             "|      /|\      ",
+             "|      / \      ",
+             "|               "
+             ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+
+# http://tinyurl.com/ztrp5jc
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+
+# http://tinyurl.com/zqklqxo
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("You lose! It was {}."
+              .format(word))
+
+# http://tinyurl.com/h9q2cpc
+hangman("cat")
